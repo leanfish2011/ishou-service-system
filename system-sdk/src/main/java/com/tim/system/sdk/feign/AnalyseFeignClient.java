a2b64f46dc4a03070372f92ae2fa14cac761dcf8
yangtzeyufei <yangtze_yufei@163.com>
2021-01-16 16:04:39 +0800

    feat(feign):增加文本检查feign

---
+++ system-sdk/src/main/java/com/tim/system/sdk/feign/AnalyseFeignClient.java
@@ -0,0 +1,16 @@
+package com.tim.system.sdk.feign;
+
+import com.tim.config.FeignConfiguration;
+import com.tim.message.Message;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "${ishou-service-system.name:ishou-service-system}", path = "/api/v1/system/analyse", configuration = FeignConfiguration.class)
+public interface AnalyseFeignClient {
+
+  @RequestMapping(value = "/text", method = RequestMethod.POST)
+  Message<Boolean> text(@RequestBody String content);
+
+}
